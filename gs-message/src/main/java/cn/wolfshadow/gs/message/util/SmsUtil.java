ae1db1f299ffb59e3d6a1ee53c8a5445436d3cde
ServerZC <594962949@qq.com>
2020-09-18 10:03:24 +0800

    删除无用的代码

--- gs-message/src/main/java/cn/wolfshadow/gs/message/util/SmsUtil.java
+++ gs-message/src/main/java/cn/wolfshadow/gs/message/util/SmsUtil.java
@@ -16,33 +16,6 @@ import java.util.List;
 @Slf4j
 public class SmsUtil {
 
-    public static void main(String[] args) {
-        String regionId = "cn-hangzhou";
-        String accessKeyId = "LTAI4G3ixVgwT2P1WUyAzrDV";
-        String accessSecret = "OVQ1UVd9URinghkGCb30GWJdhpbmoC";
-        DefaultProfile profile = DefaultProfile.getProfile(regionId, accessKeyId, accessSecret);
-        IAcsClient client = new DefaultAcsClient(profile);
-
-        CommonRequest request = new CommonRequest();
-        request.setSysMethod(MethodType.POST);
-        request.setSysDomain("dysmsapi.aliyuncs.com");
-        request.setSysVersion("2017-05-25");
-        request.setSysAction("SendSms");
-        request.putQueryParameter("RegionId", "cn-hangzhou");
-        request.putQueryParameter("PhoneNumbers", "13408102374");
-        request.putQueryParameter("SignName", "WolfShadow");
-        request.putQueryParameter("TemplateCode", "SMS_202750046");
-        request.putQueryParameter("TemplateParam", "{ 	\"code\": \"999999\" }	");
-        try {
-            CommonResponse response = client.getCommonResponse(request);
-            System.out.println(response.getData());
-        } catch (ServerException e) {
-            e.printStackTrace();
-        } catch (ClientException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static String sendSms(List<String> phoneNums, String regionId, String accessKeyId, String accessSecret,
                                String sysDomain, String sysVersion,
                                String singName, String templateCode,
